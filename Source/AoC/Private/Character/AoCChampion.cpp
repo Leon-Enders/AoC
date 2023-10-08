f978d63e0242494581c1227ac018a03b7912d50f
Leon Enders <m.enders.leon@gmail.com>
2023-10-08 12:20:49 +0200

    Added Champion class inheriting from CharacterBase

---
+++ Source/AoC/Private/Character/AoCChampion.cpp
@@ -0,0 +1,5 @@
+// Created by Leon Enders
+
+
+#include "Character/AoCChampion.h"
+
---
+++ Source/AoC/Public/Character/AoCChampion.h
@@ -0,0 +1,17 @@
+// Created by Leon Enders
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AoCCharacterBase.h"
+#include "AoCChampion.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AOC_API AAoCChampion : public AAoCCharacterBase
+{
+	GENERATED_BODY()
+	
+};
