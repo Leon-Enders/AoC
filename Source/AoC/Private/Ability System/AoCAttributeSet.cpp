ab5b495c7bbc2df9911df7047d5d2f303a9e69df
Leon Enders <m.enders.leon@gmail.com>
2023-10-15 12:16:57 +0200

    added GamplayTags and Gameplaytasks to build.cs, implemented AOC Attribute Set with Health and Mana

--- Source/AoC/Private/Ability System/AoCAttributeSet.cpp
+++ Source/AoC/Private/Ability System/AoCAttributeSet.cpp
@@ -2,8 +2,43 @@
 
 
 #include "Ability System/AoCAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
 UAoCAttributeSet::UAoCAttributeSet()
 {
 	
 }
+
+void UAoCAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UAoCAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAoCAttributeSet, HealthMax, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAoCAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAoCAttributeSet, ManaMax, COND_None, REPNOTIFY_Always);
+}
+
+void UAoCAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAoCAttributeSet, Health, OldHealth);
+	
+}
+
+void UAoCAttributeSet::OnRep_HealthMax(const FGameplayAttributeData& OldHealthMax)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAoCAttributeSet, HealthMax, OldHealthMax);
+}
+
+void UAoCAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAoCAttributeSet, Mana, OldMana);
+}
+
+void UAoCAttributeSet::OnRep_ManaMax(const FGameplayAttributeData& OldManaMax)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAoCAttributeSet, ManaMax, OldManaMax);
+}
+
+
--- Source/AoC/Public/Ability System/AoCAttributeSet.h
+++ Source/AoC/Public/Ability System/AoCAttributeSet.h
@@ -16,9 +16,33 @@ class AOC_API UAoCAttributeSet : public UAttributeSet
 
 public:
 	UAoCAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
-private:
+	
+	// RepNotify Functions
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth);
 
-	UPROPERTY(BlueprintReadOnly)
+	UFUNCTION()
+	void OnRep_HealthMax(const FGameplayAttributeData& OldHealthMax);
+	
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana);
+	
+	UFUNCTION()
+	void OnRep_ManaMax(const FGameplayAttributeData& OldManaMax);
+
+	// AttributeData
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing="OnRep_Health")
 	FGameplayAttributeData Health;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing="OnRep_HealthMax")
+	FGameplayAttributeData HealthMax;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing="OnRep_Mana")
+	FGameplayAttributeData Mana;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing="OnRep_ManaMax")
+	FGameplayAttributeData ManaMax;
+	
 };
